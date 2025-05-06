f8f8133eab115d4f73f0f531e5b7fe56dcdf9e4a
DeividPatino <patinodeivid4@gmail.com>
2025-05-05 22:11:59 -0500

    Update registro.php

--- Model/registro.php
+++ Model/registro.php
@@ -14,4 +14,13 @@ $dr = $_POST['cont'];
 $query = "INSERT INTO registro(`nombre`, `apellido`, `correo`, `contraseña`)
 VALUES('". mysql_real_string($connection_obj, $ar)."', '". mysql_real_string($connection_obj, $br) ."', '". mysql_real_string($connection_obj, $cr) ."', '". mysql_real_string($connection_obj, $dr) ."')";
 
+$res = mysql_query($connection_obj, $query);
+if($res){
+    echo "<h4>Datos insertados correctamente</h4>"
+}else{
+    echo "<h4>Error al insertar datos</h4>"
+}
+
+mysqli_close($connection_obj);
+
 ?>
