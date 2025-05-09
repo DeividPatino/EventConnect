b8beae1312fcfb52f3a5f4507c084f16ccae6755
DeividPatino <patinodeivid4@gmail.com>
2025-05-09 17:28:06 -0500

    Update registro.php

--- Model/registro.php
+++ Model/registro.php
@@ -15,16 +15,19 @@ $er = $_POST['tipo'];
 $checkQuery = "SELECT id FROM registro WHERE correo = '". mysqli_real_escape_string($connection_obj, $cr) ."'";
 $checkResult = mysqli_query($connection_obj, $checkQuery);
 
-$query = "INSERT INTO registro(`nombre`, `apellido`, `correo`, `contraseña`, `tipo`)
-VALUES('". mysqli_real_escape_string($connection_obj, $ar)."', '". mysqli_real_escape_string($connection_obj, $br) ."', '". mysqli_real_escape_string($connection_obj, $cr) ."', '". mysqli_real_escape_string($connection_obj, $dr) ."', '". mysqli_real_escape_string($connection_obj, $er) ."')";
-
-$res = mysqli_query($connection_obj, $query);
-if($res){
-    echo "<h4>Datos insertados correctamente</h4>";
-}else{
-    echo "<h4>Error al insertar datos</h4>";
+if(mysqli_num_rows($checkResult) > 0) {
+    // Si el correo ya existe, muestra un mensaje de error
+    echo "<h4>Error: El correo ya está registrado.</h4>";
+} else {
+    // Si el correo no existe, proceder con la inserción
+    $query = "INSERT INTO registro(`nombre`, `apellido`, `correo`, `contraseña`, `tipo`)
+    VALUES('". mysqli_real_escape_string($connection_obj, $ar)."', '". mysqli_real_escape_string($connection_obj, $br) ."', '". mysqli_real_escape_string($connection_obj, $cr) ."', '". mysqli_real_escape_string($connection_obj, $dr) ."', '". mysqli_real_escape_string($connection_obj, $er) ."')";
+    
+    $res = mysqli_query($connection_obj, $query);
+    if($res){
+        echo "<h4>Datos insertados correctamente</h4>";
+    }else{
+        echo "<h4>Error al insertar datos</h4>";
+    }
 }
-
-mysqli_close($connection_obj);
-
 ?>
